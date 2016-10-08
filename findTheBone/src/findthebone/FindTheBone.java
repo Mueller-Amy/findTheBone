06e64dd87930cd228ef7b727eba047bcd22f9e77
Jean Oliveira <oli15014@byui.edu>
2016-10-08 20:44:15 -0300

    8/10/2016 - Assign values to each of the instance variables using the “setter” methods, c. Call the toString() function and display the contents to the console.      Class tested: Clue , Item and ItemsWithin.

--- findTheBone/src/findthebone/FindTheBone.java
+++ findTheBone/src/findthebone/FindTheBone.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,8 +9,8 @@ import byui.cit260.findTheBone.model.Clue; // Jean Oliveira
 import byui.cit260.findTheBone.model.Game; // Keith Downing
 import byui.cit260.findTheBone.model.Item; // Jean Oliveira
 import byui.cit260.findTheBone.model.ItemsWithin; // Jean Oliveira
-import byui.cit260.findTheBone.model.Location; // Jean Oliveira
-import byui.cit260.findTheBone.model.Map; // Jean Oliveira
+import byui.cit260.findTheBone.model.Location; // 
+import byui.cit260.findTheBone.model.Map; // 
 import byui.cit260.findTheBone.model.Player; // Amy Mueller
 import byui.cit260.findTheBone.model.TownsPeople; // Keith Downing
 
@@ -35,16 +35,43 @@ public class FindTheBone {
         Map mapOne = new Map();
         Location locationOne = new Location();
         
-        
-        //Class Player set test
+        // ==============Class Assign Values ==========
+         
+        //Class Player Assign value test
         playerOne.setName("Fred Flinstone");
         playerOne.setBestTime(7.00);
         
-               
+        //Class Clue Assign value test - by Jean Oliveira
+        clueOne.setAttribute("Giraffes suggest the tigers. Cats stick together.");
+         
+        //Class Item Assign value test - by Jean Oliveira
+        itemOne.setInventory("Fish");
+        itemOne.setItemsRemaining("10");
+        
+        //Class ItemsWithin Assign value test - by Jean Oliveira
+        itemsWithinOne.setNoOfItems(40);          
+        
+        //=============== Class print out Tests ============
         
         //Class Player print out test
         String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
+        System.out.println(playerInfo);     
+                       
+        //Class Clue print out test - by Jean Oliveira
+        String clueDesc = clueOne.toString();
+        System.out.println(clueDesc);
+        
+        //Class Item  print out test - by Jean Oliveira
+        String itemInfo = itemOne.toString();
+        System.out.println(itemInfo);
+        
+        //Class ItemsWithin print out test - by Jean Oliveira
+        String itemsWithinInfo = itemsWithinOne.toString();
+        System.out.println(itemsWithinInfo);
+        
+        
+        
+        
         
              
         
