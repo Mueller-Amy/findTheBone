31bf3880ce5013dfe45f8fcc5ad5691d705b9a0d
Keith Downing <Keith Downing@DESKTOP-N09VR5S.attlocal.net>
2016-10-29 12:32:26 -0400

    10-20-16 Control added to Source Packages 10-21-16 Volume of Cylinder added. 10-21-16 Volume of Cylinder Test added. 10-21-16 Remaining team tests added. 10-22-16 Re-ran test again. 10-29-16 up to page 41 Lesson 07

--- findTheBone/src/citbyui/cit260/findTheBone/view/StartProgramView.java
+++ findTheBone/src/citbyui/cit260/findTheBone/view/StartProgramView.java
@@ -173,6 +173,51 @@ public class StartProgramView {
         mainMenuView.displayMainMenuView();
     
     }
+    public class MainMenuView {
+        private String menu;
 
+        public MainMenuView() {
+            this.menu = "\n"
+                      + "\n=========================================="
+                      + "\n| Main Menu                              |"
+                      + "\n=========================================="
+                      + "nN - Start New Game"
+                      + "nL - Load Saved Game"
+                      + "nS - Save Game"
+                      + "nH - Help"
+                      + "nX - Exit"
+                      + "\n==========================================";
+        }
+        public void displayMainMenuView(){
+            boolean done = false; // set flag to not done
+            do {
+                // prompt for and get players name
+                String menuOption = this.getMenuOption();
+                if (menuOption.toUpperCase().equals("Q")) //user wants to quit
+                    return; // Exit game
+                
+                // Do the requested action and display the next view
+                done = this.doAction(menuOption);
+                
+        }while (!done);
+        
+            }
+
+        private String getMenuOption() {
+            System.out.println("\n*** getMenuOption() function called ***");
+            return "N";
+        }
+
+        private boolean doAction(String menuOption) {
+            System.out.println("\n*** doAction() function called ***");
+            return true;
+        }
+        
+        
+        }
+        
+            
+                
+    }
    
-}    
+
