523bb4bb2fbaa9f32e37add0d1f167b5843e5c74
Home <Home@192.168.11.101>
2016-11-03 18:52:26 -0600

    10/9/2016 areaofBox

--- findTheBone/src/citbyui/cit260/findTheBone/view/AreaOfBoxView.java
+++ findTheBone/src/citbyui/cit260/findTheBone/view/AreaOfBoxView.java
@@ -11,4 +11,26 @@ package citbyui.cit260.findTheBone.view;
  */
 public class AreaOfBoxView {
     
+    public String getInput() {
+    
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        String selection = null;
+        
+        //while a valid name has not been retrieved
+        while (!valid) {
+            
+            //get the value entered from the keyboard
+            selection = keyboard.netLine();
+            selection = selection.trim();
+            
+            if (selection.length() <1) {//blank value entered
+                System.out.println("\n*** Invalid selection. Try again***");
+                continue;
+            }
+            break;
+    }
+            return selection; //return the name
+    }
+    
 }
