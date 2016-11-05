eb64a1ae97cf32bb2cced9ff647651df393b58ca
Home <Home@192.168.11.101>
2016-11-05 16:58:52 -0600

    10/9/2016 areaofBox

--- findTheBone/src/citbyui/cit260/findTheBone/view/VolumeOfBoxView.java
+++ findTheBone/src/citbyui/cit260/findTheBone/view/VolumeOfBoxView.java
@@ -4,60 +4,58 @@
  * and open the template in the editor.
  */
 package citbyui.cit260.findTheBone.view;
-
+import java.util.Scanner;
 /**
  *
  * @author Amy Mueller
  */
 public class VolumeOfBoxView {
-   
+   String promptMessages = "\n"
+           "+\n=================="
+           "+\n| Enter the width of the box:"
+           "+\n============================"
+           "+\n| Enter the length of the box:"
+           "+\n============================"
+           "+\n| Enter the height of the box:"
+           "+\n=============================";
     
     public String getInput() {
     
         Scanner keyboard = new Scanner(System.in);
+        String value = "";
         boolean valid = false;
-        String selection = null;
         
         
         
         //while a valid name has not been retrieved
         while (!valid) {
             
-            System.out.print("\n Enter the length of the box: ");
-            Scanner length = new Scanner (System.in);
+            System.out.print("\n" + this.promptMessages);
+            
             //get the value entered from the keyboard
-            selection = keyboard.netLine();
-            selection = selection.trim();
+            value = keyboard.nextLine();
+            value = value.trim();
          
             
-            if (selection.length() <1) {//blank value entered
+            if (value.length() <1) {//blank value entered
                 System.out.println("\n*** Invalid selection. Try again***");
                 continue;
-            System.out.print('\n Enter the width of the box:");
-            Scanner length = new Scanner (System.in);
-            selection = keyboard.netLine ();
-            selection = selection.trim();
-              if (selection.length() <1) {//blank value entered
-                System.out.println("\n*** Invalid selection. Try again***");
-                continue;
-              }
-           System.out.print("\n Enter the height of the box:");
-           Scanner length = new Scanner (System.in);
-           selection = keyboard.netLine ();
-           selection = selection.trim();
-                if (selection.height()< 1) {
-                    System.out.println ("\n *** Invalid selection. Try again");
-                    continue;
-                }
-            
-            System.out.println("\n The volume of the box is: " + calcVolumeOfBox(double length, double width, double height) + "\n";
             }
             break;
-    }
-            return selection; //return the name
-    }
-    
+        }
+            return value;
+        }
+            
 }
+            
+          //  System.out.println("\n The volume of the box is: " + calcVolumeOfBox(double length, double width, double height) + "\n";
+         //   }
+         //   break;
+    //}
+   //         return selection; //return the name
+  //  }
+    
+//}
 
 
 
