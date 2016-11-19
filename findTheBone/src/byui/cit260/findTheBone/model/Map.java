e189b4519bb2ea11f659baa3e323426e9be9338f
Jean Oliveira <oli15014@byui.edu>
2016-11-18 22:38:13 -0300

    Create overload Map().....page 26 instructions.

--- findTheBone/src/byui/cit260/findTheBone/model/Map.java
+++ findTheBone/src/byui/cit260/findTheBone/model/Map.java
@@ -32,10 +32,37 @@ public class Map implements Serializable{
     public Map() {
     }
 
-    public Map(int i, int i0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     *
+     * @param noOfRows
+     * @param noOfColumns
+     */
+    public Map(int noOfRows, int noOfColumns) {
+    
+    if (noOfRows<1 || noOfColumns<1) {
+        System.out.println("The number of rows and columns must be > zero");
+        return;
+    }
+    this.noOfRows=noOfRows;
+    this.noOfColumns=noOfColumns;
+    
+    //create 2-D array for Locations objects
+    this.locations=new Location[noOfRows][noOfColumns];
+    
+    for (int row=0;row < noOfRows; row++) {
+        for (int column=0;column < noOfColumns; column++) {
+            //create and initialize new Location object instance
+            Location location=new Location();
+            location.setColumn(column);
+            location.setRow(row);
+            location.setVisited(false);
+            
+            //assign the Location object to the current position in  array
+            locations[row][column]=location;
+        }
     }
     
+}
     
  
     
